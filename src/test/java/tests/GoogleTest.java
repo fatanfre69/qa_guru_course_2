ff2d51218daa074475975d86e4cbf4eb8f26fb11
Andrey Kulikov <9230697@gmail.com>
2020-07-28 14:07:01 +0300

    New

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byTitle;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,4 +20,36 @@ class GoogleTest {
         // Проверить, что Selenide появился в результатах поиска
         $("html").shouldHave(text("ru.selenide.org"));
     }
+
+
+
+    @Test
+    void visaUsaSearchTest() {
+//        Открыть Google
+        open("https://google.com");
+
+//        Ввести "visa USA"
+        $(byTitle("Поиск")).setValue("visa USA");
+
+//        Клик по Поиску
+        $(byName("btnK")).click();
+
+//        Проверить, что visa USA появился в результате поиска
+        $("body").shouldHave(text("ru.usembassy.gov"));
+    }
+
+
+
+    @Test
+    void longSearchTest() {
+//        Открыть Google
+        open("https://google.com");
+
+//        Ввести "Как мне открыть банку варенья без открывашки"
+        $(byName("q")).setValue("Как мне открыть банку варенья без открывашки").pressEnter();
+
+//        Проверить, что в результате поиска есть заданный текст
+        $("body").shouldBe(text("Как открыть банку с вареньем"));
+    }
+
 }
---
+++ src/test/java/tests/YandexTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTest {
+
+  @Test
+    void googleSearchTest(){
+//      открыть Yandex
+      open("https://yandex.ru");
+
+//      ввести в поиск Google
+      $("#text").val("Google").pressEnter();
+
+//      проверить, что Google есть в результате поиска
+      $("html").shouldBe(Condition.text("Google — поисковая система"));
+
+  }
+
+
+
+}
