2f22cb2faa47626dfe926f4b50ff826cc444ccef
Andrey Kulikov <9230697@gmail.com>
2020-09-24 15:20:36 +0300

    1

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.*;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelpers.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+public class TestBase {
+
+    @BeforeAll
+    @Step("Tests setup")
+    public static void beforeAll() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+//        Configuration.browser = CustomWebDriver.class.getName();
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getBrowserConsoleLogs());
+//        attachVideo();
+        closeWebDriver();
+    }
+
+    public static String getBrowserConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+    }
+
+}
--- src/test/java/tests/lesson_12_attach_video/attachVideoTest.java
+++ src/test/java/tests/lesson_12_attach_video/attachVideoTest.java
@@ -1,16 +1,17 @@
 package tests.lesson_12_attach_video;
 
 import org.junit.jupiter.api.*;
+import tests.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.*;
 
-public class attachVideoTest {
+public class attachVideoTest extends TestBase {
 
     @Test
     @Tag("attach_video")
-    public void attachVideoTests() {
+    public void attachVideoTests()  {
 //        Configuration.headless = true;
 
             open("https://google.com");
