b9acadbec058780fbbd10797a840e81272ab75bb
Andrey Kulikov <9230697@gmail.com>
2020-09-24 15:59:21 +0300

    1

--- src/test/java/WebDriverProvider/WebDriverProvider.java
+++ src/test/java/WebDriverProvider/WebDriverProvider.java
@@ -1,18 +1,25 @@
 package WebDriverProvider;
 
-//public class WebDriverProvider implements Supplier<WebDriver> {
-//
-//    @Override
-//    public WebDriver get() {
-//        final String browserName = System.getProperty("browser.name");
-//        if ("chrome".equals(browserName)) {
-//            WebDriverManager.chromedriver().setup();
-//            return new ChromeDriver();
-//        }
-//        if ("firefox".equals(browserName)) {
-//            WebDriverManager.firefoxdriver().setup();
-//            return new FirefoxDriver();
-//        }
-//        throw new RuntimeException("Unknown browser name: " + browserName);
-//    }
-//}
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.function.Supplier;
+
+public class WebDriverProvider implements Supplier<WebDriver> {
+
+    @Override
+    public WebDriver get() {
+        final String browserName = System.getProperty("browser.name");
+        if ("chrome".equals(browserName)) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        }
+        if ("firefox".equals(browserName)) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
+        throw new RuntimeException("Unknown browser name: " + browserName);
+    }
+}
